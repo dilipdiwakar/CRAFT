bc785bd9a356bd94e0304a4e7dcac66678a8c633
dilipdiwakar <dilip.diwakar@gmail.com>
2016-06-28 22:00:58 -0700

    My Craft initial version

---
+++ src/test/java/com/craft/WebAppTest.java
@@ -0,0 +1,20 @@
+package com.craft;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class WebAppTest 
+{
+
+	@Test
+	public void webSiteTest()
+	{
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://lendingclub.com");
+		driver.quit();
+	}
+}
